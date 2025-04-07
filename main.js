5cbbb9fd75f8978999289f945f8b1a7534c9008f
Bobir118 <bobir3436@gmail.com>
2025-04-07 19:44:17 +0200

    mobile

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ let menu = document.querySelector(".fixed-menu");
 
 menu.addEventListener('click', function(event){
     let clickTarget = event.target;
-    let activeStn = document.querySelector('.active');
-
+    let activeBth = document.querySelector('.active');
+if (clickTarget.classList.contains('active')){
+    return
+}
     if (clickTarget.classList.contains('nav-link')){
         clickTarget.classList.add('active');
         activeBth.classList.remove('active');
     }
 });
 
-let classList = 'main-link';
+let classLink = '.main-link';
 window.onscroll = function() {
     let h = document.documentElement.clientHeight;
     if (window.scrollY >= h*4){
